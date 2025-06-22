3d03184884cc78662794f19af61135c27506188c
hjhhjh111 <15687903413@139.COM>
2025-06-23 00:37:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ noButton.addEventListener("click", function () {
 });
 
 // Yes 按钮点击后，进入表白成功页面
-const loveTest = 恭喜你获得现金大奖！！！！ ( >᎑<)♡︎ᐝ  ${
+const loveTest = 恭喜你获得现金大奖！！！ ( >᎑<)♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
 
